f826963e531c304c64a48ddfafc37b8d1b688cab
W1nU <jsw9808@gmail.com>
2022-04-29 19:46:01 +0900

    feat : storage structure

--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -1,18 +1,66 @@
 import { atom, RecoilState } from "recoil";
 
+interface IPost {
+  id: number;
+  title: string;
+  content: string;
+  hostEmail: string;
+  images: string[];
+  fromDate: Date;
+  toDate: Date;
+  postNumber: string;
+  address: string;
+  detailedAddress: string;
+  latitude: string;
+  longitude: string;
+  limit: number; // 최대 참가 가능 인원 수
+}
+interface IUser {
+  email: string;
+  password: string;
+  name: string;
+}
+
+interface IUserWrap {
+  [key: string]: IUser;
+}
+interface IReservation {
+  userEmail: string;
+  reservedPostId: number;
+  number: number;
+}
 export interface IState {
-  userEmail: RecoilState<string>;
-  userName: RecoilState<string>;
+  users: RecoilState<IUserWrap>;
+  currentUser: RecoilState<IUser> | RecoilState<null>;
+  posts: RecoilState<IPost[]>;
+  reservation: RecoilState<IReservation[]>;
 }
 
 const storage: IState = {
-  userEmail: atom({
-    key: "userEmail",
-    default: "jsw9808@gmail.com",
+  users: atom({
+    key: "user",
+    default: {
+      "jsw9808@gmail.com": {
+        email: "jsw9808@gmail.com",
+        password: "1234",
+        name: "정승우",
+      },
+    } as IUserWrap,
   }),
-  userName: atom({
-    key: "userName",
-    default: "",
+
+  currentUser: atom({
+    key: "currentUser",
+    default: null,
+  }),
+
+  posts: atom({
+    key: "posts",
+    default: [] as IPost[],
+  }),
+
+  reservation: atom({
+    key: "reservation",
+    default: [] as IReservation[],
   }),
 };
 
